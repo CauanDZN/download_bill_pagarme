f43cb37eef3fa30f713053c2efab78d85b1603ad
Cauan Victor <cauanvictor0325@gmail.com>
2025-07-09 08:59:23 -0300

    fix: ajustar argumentos de configuração do Hypercorn para permitir IPs encaminhados

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       name: "app",
       script: "./venv/bin/hypercorn",
-      args: "app:app --bind 0.0.0.0:5000 --forwarded-allow-ips='*'",
+      args: "app:app --bind 0.0.0.0:5000",
       interpreter: "none",
       cwd: "/home/cauan/download_bill_pagarme",
       env: {
-        PATH: "/home/cauan/download_bill_pagarme/venv/bin:$PATH"
+        PATH: "/home/cauan/download_bill_pagarme/venv/bin:$PATH",
+        HYPERCORN_FORWARD_ALLOW_IPS: "*"
       }
     }
   ]
