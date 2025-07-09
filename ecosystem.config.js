ffa704d018cbe3fbb22aba69221d9eae7c25aa4d
Cauan Victor <cauanvictor0325@gmail.com>
2025-07-09 08:57:52 -0300

    feat: adicionar configuração do ecosystem para gerenciamento de aplicativos com Hypercorn

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./venv/bin/hypercorn",
+      args: "app:app --bind 0.0.0.0:5000 --forwarded-allow-ips='*'",
+      interpreter: "none",
+      cwd: "/home/cauan/download_bill_pagarme",
+      env: {
+        PATH: "/home/cauan/download_bill_pagarme/venv/bin:$PATH"
+      }
+    }
+  ]
+}
